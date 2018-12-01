bd4526893b6dd344ea1db64c6feac75ecfc874da
Laszlo Sitzer <laszlo.sitzer@sumup.com>
2018-12-01 18:22:44 +0100

    Implement mcc_from_plmn().

--- dec.py
+++ dec.py
@@ -8,3 +8,14 @@ def hexstr_to_intarr(s):
 		v = int(tmp, 16)
 		a.append(v)
 	return a
+
+# Accepts hex string representing three bytes
+def mcc_from_plmn(plmn):
+	ia = hexstr_to_intarr(plmn)
+	digit1 = ia[0] & 0x0F		# 1st byte, LSB
+	digit2 = (ia[0] & 0xF0) >> 4	# 1st byte, MSB
+	digit3 = ia[1] & 0x0F		# 2nd byte, LSB
+	mcc = digit1 * 100
+	mcc += digit2 * 10
+	mcc += digit3
+	return mcc
--- dec_test.py
+++ dec_test.py
@@ -8,5 +8,8 @@ class DecTestCase(unittest.TestCase):
 	def testHexStringToIntArray(self):
 		self.assertEqual(dec.hexstr_to_intarr("ff1020"), [255, 16, 32])
 
+	def testDecMCCfromPLMN(self):
+		self.assertEqual(dec.mcc_from_plmn("92f501"), 295)
+
 if __name__ == "__main__":
 	unittest.main()

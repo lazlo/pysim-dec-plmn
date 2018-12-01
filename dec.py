ca147220636cf653e7d557645c61920ca154f4fd
Laszlo Sitzer <laszlo.sitzer@sumup.com>
2018-12-01 11:15:54 +0100

    Add offset var in hexstr_to_intarr().

--- dec.py
+++ dec.py
@@ -1,8 +1,9 @@
 def hexstr_to_intarr(s):
 	a = []
 	for i in range(0, len(s) / 2):
-		msb_nib = s[2 * i]
-		lsb_nib = s[(2 * i) + 1]
+		offset = 2 * i
+		msb_nib = s[offset]
+		lsb_nib = s[offset + 1]
 		tmp = msb_nib + lsb_nib
 		v = int(tmp, 16)
 		a.append(v)
